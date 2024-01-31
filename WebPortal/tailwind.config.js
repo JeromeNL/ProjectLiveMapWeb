4667266ef2db3bc55ad2c63867b685b0d68816f2
Mauro <mr.weyers@student.avans.nl>
2024-01-31 20:34:53 +0100

    Added Tailwind and daisyui

---
+++ WebPortal/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Views/**/*.{html,js,cshtml}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
