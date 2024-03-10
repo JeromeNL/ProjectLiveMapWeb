cac6d4dd7a8d6ec5cc83fa6929b654b40c2265a4
Lamineslot <slotlamine@gmail.com>
2024-03-10 15:25:41 +0100

    Added FacilityReportController
    (cherry picked from commit 38180d2515f5f5d0f5d5f7b6ec402e2d194aa5a7)

---
+++ WebPortal/Controllers/FacilityReportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPortal.Controllers;
+
+public class FacilityReportController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
