b5c464dfffac07757ea83bd4c70c8745a2ec34bf
tovertractor <as.ansano@student.avans.nl>
2024-03-14 14:51:41 +0100

    adding database with api for users

--- MobileMapAPI/Endpoints/UserEndpoints.cs
+++ MobileMapAPI/Endpoints/UserEndpoints.cs
@@ -1,9 +1,32 @@
-﻿namespace MobileMapAPI.Endpoints;
+﻿using DataAccess;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MobileMapAPI.Endpoints;
 
 public class UserEndpoints
 {
     public static void MapUserEndpoints(IEndpointRouteBuilder app)
     {
+        app.MapGet("/users", (LiveMapDbContext context) =>
+        {
+            var users = context.Users.ToList();
+            return Results.Ok(users);
+        });
         
+        app.MapGet("/users/{name}", (string name, LiveMapDbContext context) =>
+        {
+            var users = context.Users.ToList();
+            foreach (User user in users)
+            {
+                if (user.Name == name)
+                {
+                    return Results.Ok(user);
+                }    
+            }
+
+            return Results.NotFound("User not found");
+        });
     } 
 }
