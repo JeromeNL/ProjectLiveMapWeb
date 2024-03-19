67f110f49424e051039b740fdd0a934442e999df
Lamineslot <slotlamine@gmail.com>
2024-03-19 23:47:37 +0100

    Added migration for seeddata update

--- DataAccess/Migrations/20240319212234_Changed_seeddata_so_it_is_more_realistic.Designer.cs
+++ DataAccess/Migrations/20240319212234_Changed_seeddata_so_it_is_more_realistic.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(LiveMapDbContext))]
-    partial class LiveMapDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240319212234_Changed_seeddata_so_it_is_more_realistic")]
+    partial class Changed_seeddata_so_it_is_more_realistic
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
