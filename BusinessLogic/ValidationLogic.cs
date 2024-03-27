129d91216d8573473d25228f935abd3d768e76c6
Lamineslot <slotlamine@gmail.com>
2024-03-27 11:42:56 +0100

    Use mapbox and Efteling instead of open street map and Vennenbos

--- BusinessLogic/ValidationLogic.cs
+++ BusinessLogic/ValidationLogic.cs
@@ -2,25 +2,25 @@
 
 public static class ValidationLogic
 {
-    private static readonly List<(double Latitude, double Longitude)> points =
-        new List<(double Latitude, double Longitude)>()
-        {
-            (51.3422208920495, 5.241121225493207),
-            (51.34488819214301, 5.25017375204897),
-            (51.3414702670499, 5.257591469196808),
-            (51.33793144075602, 5.249741910731706)
-        };
+    private static readonly List<(double Latitude, double Longitude)> Points =
+    [
+        (51.653202521428078, 5.0413749255922244),
+        (51.653721716281296, 5.0550985543836768),
+        (51.64661222380488, 5.0575641086431933),
+        (51.646425817616468, 5.0443668045587984),
+        (51.649581304758911, 5.0386373727005704)
+    ];
     
     public static bool IsPointInsidePolygon(double latitude, double longitude)
     {
-        int numIntersections = 0;
-        double tolerance = 1e-6;
-        for (int i = 0; i < points.Count; i++)
+        var numIntersections = 0;
+        const double tolerance = 1e-6;
+        for (var i = 0; i < Points.Count; i++)
         {
-            double x1 = points[i].Item1;
-            double y1 = points[i].Item2;
-            double x2 = points[(i + 1) % points.Count].Item1;
-            double y2 = points[(i + 1) % points.Count].Item2;
+            var x1 = Points[i].Item1;
+            var y1 = Points[i].Item2;
+            var x2 = Points[(i + 1) % Points.Count].Item1;
+            var y2 = Points[(i + 1) % Points.Count].Item2;
 
             if (Math.Abs(latitude - x1) < tolerance && Math.Abs(longitude - y1) < tolerance)
                 return true;
