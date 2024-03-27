b161ddd605f4b7b77d91ebc9f617988556da24c1
Lamineslot <slotlamine@gmail.com>
2024-03-27 11:42:13 +0100

    Reverted merge conflict issues

--- DataAccess/DataSeeder.cs
+++ DataAccess/DataSeeder.cs
@@ -16,24 +16,6 @@ public static class DataSeeder
         modelBuilder.Entity<Facility>().HasData(facilities);
         modelBuilder.Entity<ProposedFacility>().HasData(proposedFacilities);
         modelBuilder.Entity<FacilityReport>().HasData(facilityReports);
-        modelBuilder.Entity<User>().HasData(GenerateUsers());
-    }
-
-    private static List<User> GenerateUsers()
-    {
-        var users = new List<User>()
-        {
-            new()
-            {
-                Id = -1,
-                Name = "Almior"
-            },
-            new()
-            {
-                Id = -2,
-                Name = "Joram"
-            }
-        };
-        return users;
+        modelBuilder.Entity<User>().HasData(users);
     }
 }
