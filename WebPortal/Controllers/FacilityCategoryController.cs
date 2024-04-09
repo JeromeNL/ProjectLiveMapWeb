f063b93786db2203a1a493526e810d81791486a8
Lamineslot <71119665+Lamineslot@users.noreply.github.com>
2024-04-09 19:41:42 +0200

    Added views and controller methods for basic category CRUD

---
+++ WebPortal/Controllers/FacilityCategoryController.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebPortal.Controllers;
+
+public class FacilityCategoryController(LiveMapDbContext context) : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        var facilityCategories = await context.FacilityCategories.ToListAsync();
+        return View(facilityCategories);
+    }
+    
+    public IActionResult Create()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(FacilityCategory facilityCategory)
+    {
+        if (!ModelState.IsValid) return View(facilityCategory);
+        await context.FacilityCategories.AddAsync(facilityCategory);
+        await context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+    
+    public async Task<IActionResult> Edit(int id)
+    {
+        var facilityCategory = await context.FacilityCategories.FindAsync(id);
+        if (facilityCategory == null) return NotFound();
+        return View(facilityCategory);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Edit(FacilityCategory facilityCategory)
+    {
+        if (!ModelState.IsValid) return View(facilityCategory);
+        context.FacilityCategories.Update(facilityCategory);
+        await context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var facilityCategory = await context.FacilityCategories.FindAsync(id);
+        if (facilityCategory == null) return NotFound();
+        context.FacilityCategories.Remove(facilityCategory);
+        await context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+}
