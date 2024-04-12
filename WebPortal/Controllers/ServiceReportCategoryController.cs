137c5f623350324024140eb929eb72526fa49f8a
tovertractor <as.ansano@student.avans.nl>
2024-04-12 13:16:55 +0200

    adding category overview and delete

---
+++ WebPortal/Controllers/ServiceReportCategoryController.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebPortal.Controllers;
+
+public class ServiceReportCategoryController(LiveMapDbContext context) : Controller
+{
+    public IActionResult Index()
+    {
+        return View(context.ServiceReportCategories.ToList());
+    }
+    public IActionResult Delete(int id)
+    {
+        var category = context.ServiceReportCategories.Find(id);
+        if (category == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        context.ServiceReportCategories.Remove(category);
+        context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
