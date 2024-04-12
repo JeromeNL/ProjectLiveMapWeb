c8a19367e39ad04eba382fbcb7d6440139e59d6f
Lamineslot <71119665+Lamineslot@users.noreply.github.com>
2024-04-12 17:36:21 +0200

    Added FacilityReportController

---
+++ MobileMapAPI/Controllers/FacilityReportController.cs
@@ -0,0 +1,52 @@
+﻿using DataAccess;
+using DataAccess.Models.Enums;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MobileMapAPI.Controllers;
+
+[ApiController]
+[Route("facilities/facility-reports")]
+public class FacilityReportController(LiveMapDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAllFacilityReports()
+    {
+        var facilityReports = await context.FacilityReports
+            .Include(report => report.ProposedFacility)
+            .Include(report => report.User)
+            .ToListAsync();
+        return Ok(facilityReports);
+    }
+    
+    [HttpGet("{reportId:int}")]
+    public async Task<IActionResult> GetFacilityReport(int reportId)
+    {
+        var facilityReport = await context.FacilityReports
+            .Include(report => report.ProposedFacility)
+            .Include(report => report.User)
+            .FirstOrDefaultAsync(report => report.Id == reportId);
+
+        if (facilityReport == null)
+        {
+            return NotFound("Report not found");
+        }
+
+        return Ok(facilityReport);
+    }
+    
+    [HttpPatch("{reportId:int}/cancel")]
+    public async Task<IActionResult> CancelReport(int reportId)
+    {
+        var facilityReport = await context.FacilityReports.FindAsync(reportId);
+
+        if (facilityReport == null)
+        {
+            return NotFound("Report not found");
+        }
+
+        facilityReport.Status = ReportStatus.Cancelled;
+        await context.SaveChangesAsync();
+        return Ok("Report has been succesfully cancelled");
+    }
+}
