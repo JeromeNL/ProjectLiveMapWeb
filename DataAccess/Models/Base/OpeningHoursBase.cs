e7cac821579ca6890a160e762f11137eebebcc1f
Thieme <46195238+ThiemeH@users.noreply.github.com>
2024-04-13 16:41:12 +0200

    Added custom HourMinute converter

--- DataAccess/Models/Base/OpeningHoursBase.cs
+++ DataAccess/Models/Base/OpeningHoursBase.cs
@@ -1,13 +1,17 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using DataAccess.Converters;
 using DataAccess.Models.Enums;
 
 namespace DataAccess.Models.Base;
 
 public abstract class OpeningHoursBase
 {
+    [JsonConverter(typeof(HourMinuteConverter))]
     public TimeOnly OpenTime { get; set; }
+    
+    [JsonConverter(typeof(HourMinuteConverter))]
     public TimeOnly CloseTime { get; set; }
 
     [Required]
