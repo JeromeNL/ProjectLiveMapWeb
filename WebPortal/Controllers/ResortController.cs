b726c3c55597e78edb27337a9631c49cfba18acc
JeromeNL <jw.kwetters@student.avans.nl>
2024-04-18 23:19:28 +0200

    CRUD functional

--- WebPortal/Controllers/ResortController.cs
+++ WebPortal/Controllers/ResortController.cs
@@ -7,12 +7,14 @@ namespace WebPortal.Controllers;
 
 public class ResortController(LiveMapDbContext context) : Controller
 {
+    [HttpGet]
     public IActionResult Index()
     {
         var resorts = context.HolidayResorts.ToList();
         return View(resorts);
     }
 
+    [HttpPost]
     public async Task<IActionResult> Delete(int resortId)
     {
         var resort = await context.HolidayResorts.FindAsync(resortId);
@@ -24,13 +26,22 @@ public class ResortController(LiveMapDbContext context) : Controller
         return RedirectToAction("Index");
     }
 
-    public IActionResult Save(HolidayResort holidayResort)
+    [HttpPost]
+    public IActionResult Create(HolidayResort holidayResort)
     {
+       
         context.HolidayResorts.Add(holidayResort);
         context.SaveChanges();
         return RedirectToAction("Index");
     }
 
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+   [HttpGet]
     public async Task<IActionResult> Details(int resortId)
     {
         var resort = await context.HolidayResorts.FindAsync(resortId);
@@ -41,6 +52,7 @@ public class ResortController(LiveMapDbContext context) : Controller
         return RedirectToAction("Index");
     }
 
+    [HttpPost]
     public async Task<IActionResult> Update(HolidayResort holidayResort)
     {
         context.Update(holidayResort);
