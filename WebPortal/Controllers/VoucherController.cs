e9cbf4d73187d4a2b5568ddc29a4ae4b6da3de9c
tovertractor <as.ansano@student.avans.nl>
2024-05-09 11:52:17 +0200

    commit workign on wrong branch

---
+++ WebPortal/Controllers/VoucherController.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebPortal.Controllers.Base;
+
+namespace WebPortal.Controllers;
+
+public class VoucherController(LiveMapDbContext Context) : LivemapController
+{
+    public async Task<IActionResult> Index()
+    {
+        var transactions = await Context.PointsTransactions.ToListAsync();
+        return View(transactions);
+    }
+}
