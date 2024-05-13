74010cf50617c332993f3adfebe4e3b8e20b9238
tovertractor <as.ansano@student.avans.nl>
2024-05-13 15:40:16 +0200

    adding validation for price

--- WebPortal/Controllers/VoucherController.cs
+++ WebPortal/Controllers/VoucherController.cs
@@ -30,17 +30,24 @@ public class VoucherController(LiveMapDbContext Context) : LivemapController
         var user = await Context.Users.FindAsync(userId);
         var resort = await Context.HolidayResorts.FindAsync(ResortId);
         var pointsOfUser = await Context.PointsTransactions.Select(t => t.Amount).SumAsync();
+        const string returnTo = nameof(Index);
+
+        if (price <= 0)
+        {
+            TempData["ErrorMessage"] = "De opgegeven prijs is 0 of lager, vul een hogere prijs in";
+            return RedirectToAction(returnTo);
+        }
 
         if (pointsOfUser < price)
         {
             TempData["ErrorMessage"] = $"{user.Name} heeft niet genoeg punten om deze voucher te kopen";
-            return RedirectToAction("Index");
+            return RedirectToAction(returnTo);
         }
 
         if (user == null && resort == null)
         {
             TempData["ErrorMessage"] = "Gekozen gebruiker bestaat niet voor dit vakantiepark";
-            return RedirectToAction("Index");
+            return RedirectToAction(returnTo);
         }
 
         var voucher = new Voucher()
