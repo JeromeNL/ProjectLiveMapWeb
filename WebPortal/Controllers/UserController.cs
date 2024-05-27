4e9d594bbb2a9d5f7c120a1fb128f798e4aceadc
tovertractor <as.ansano@student.avans.nl>
2024-05-27 17:03:11 +0200

    possible to add users

---
+++ WebPortal/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebPortal.Controllers.Base;
+using WebPortal.Models;
+
+namespace WebPortal.Controllers;
+
+[Authorize(Roles = $"{nameof(Role.SuperAdmin)},{nameof(Role.ResortAdmin)}")]
+public class UserController(LiveMapDbContext context, UserManager<ApplicationUser> userManager) : LivemapController
+{
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return View(await getUsersWithRole());
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(RegisterViewModel viewModel)
+    {
+        var user = new ApplicationUser();
+        user.UserName = viewModel.UserName;
+        user.NormalizedUserName = viewModel.UserName.ToUpper();
+        user.HolidayResortId = ResortId;
+        user.HolidayResort = await context.HolidayResorts.FindAsync(ResortId);
+
+        var result = await userManager.CreateAsync(user, viewModel.Password);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View("Create");
+        }
+
+        var currentUser = await userManager.GetUserAsync(User);
+        var currentRole = await userManager.GetRolesAsync(currentUser);
+
+        if (currentRole.FirstOrDefault() == nameof(Role.SuperAdmin))
+        {
+            await userManager.AddToRoleAsync(user, nameof(Role.ResortAdmin));
+        }
+        else
+        {
+            await userManager.AddToRoleAsync(user, nameof(Role.ResortEmployee));
+        }
+
+        return View("Index", await getUsersWithRole());
+    }
+
+    private async Task<Dictionary<ApplicationUser, string?>> getUsersWithRole()
+    {
+        var userRoles = new Dictionary<ApplicationUser, string?>();
+        var users = await userManager.Users
+            .Where(u => u.HolidayResortId == ResortId)
+            .ToListAsync();
+
+        foreach (var user in users)
+        {
+            var role = await userManager.GetRolesAsync(user);
+            userRoles.Add(user, role.FirstOrDefault());
+        }
+
+        return userRoles;
+    }
+}
---
+++ WebPortal/Models/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPortal.Models;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "Gebruikersnaam is verplicht")]
+    public string UserName { get; set; }
+
+    [Required(ErrorMessage = "Wachtwoord is verplicht")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
