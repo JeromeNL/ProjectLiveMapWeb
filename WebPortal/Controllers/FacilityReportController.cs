d589fcce35a0993e7ce102a6f69c8b55836cf422
tovertractor <as.ansano@student.avans.nl>
2024-05-27 14:30:06 +0200

    adding controller auth for facilities categories and auth

--- WebPortal/Controllers/FacilityReportController.cs
+++ WebPortal/Controllers/FacilityReportController.cs
@@ -1,12 +1,14 @@
 ﻿using DataAccess;
 using DataAccess.Models;
 using DataAccess.Models.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebPortal.Controllers.Base;
 
 namespace WebPortal.Controllers;
 
+[Authorize(Roles = "ResortEmployee,ResortAdmin")]
 public class FacilityReportController(LiveMapDbContext context) : LivemapController
 {
     public async Task<IActionResult> Index()
