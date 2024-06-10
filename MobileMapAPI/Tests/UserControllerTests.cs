e202d454fde20ea03386e2ff31e0607e1b3f21bc
tovertractor <as.ansano@student.avans.nl>
2024-06-10 12:47:16 +0200

    writing test for all get requests

---
+++ MobileMapAPI/Tests/UserControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace MobileMapAPI.Tests;
+
+public class UserControllerTests(WebApplicationFactory<Program> factory) : ApiTest(factory)
+{
+    [Fact]
+    public async Task GetAllServiceReportsFromUser_ReturnsSuccessAndReports()
+    {
+        var response = await Client.GetAsync("/users/1/service-reports");
+
+        response.EnsureSuccessStatusCode();
+
+        var reports = await response.Content.ReadAsAsync<List<ServiceReport>>();
+        Assert.NotEmpty(reports);
+    }
+
+    [Fact]
+    public async Task GetAllFacilityReportsFromUser_ReturnsSuccessAndReports()
+    {
+        var response = await Client.GetAsync("/users/1/facility-reports");
+
+        response.EnsureSuccessStatusCode();
+
+        var reports = await response.Content.ReadAsAsync<List<FacilityReport>>();
+        Assert.NotEmpty(reports);
+    }
+
+    [Fact]
+    public async Task GetAllPointsFromUser_ReturnsSuccessAndPoints()
+    {
+        var response = await Client.GetAsync("/users/1/points/total");
+
+        response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task GetAllPointsRewardedFromUser_ReturnsSuccess()
+    {
+        var response = await Client.GetAsync("users/1/points/awarded");
+
+        response.EnsureSuccessStatusCode();
+    }
+
+    public async Task GetAllPointsDeductedFromUser_ReturnsSuccess()
+    {
+        var response = await Client.GetAsync("users/1/points/deducted");
+
+        response.EnsureSuccessStatusCode();
+    }
+}
